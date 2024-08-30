5849a80adcbe32f215c40d07c82b67e62ab258b5
benyaa <ben2460@gmail.com>
2024-08-30 15:34:24 +0300

    decoupling flow processors from db

--- definitions/flow_manager.go
+++ definitions/flow_manager.go
@@ -1,18 +1,11 @@
 package definitions
 
 import (
-	"github.com/go-streamline/interfaces/definitions/models"
 	"github.com/google/uuid"
 )
 
 type FlowManager interface {
-	GetFirstProcessorsForFlow(flowID uuid.UUID) ([]models.Processor, error)
-	GetLastProcessorForFlow(flowID uuid.UUID) (*models.Processor, error)
-	ListFlows() ([]models.Flow, error)
-	GetFlowByID(flowID uuid.UUID) (*models.Flow, error)
-	GetProcessorByID(flowID uuid.UUID, processorID uuid.UUID) (*models.Processor, error)
-	GetNextProcessors(flowID uuid.UUID, processorID uuid.UUID) ([]models.Processor, error)
-	ListProcessorsForFlow(flowID uuid.UUID) ([]models.Processor, error)
-	AddProcessorToFlowBefore(flowID uuid.UUID, processor *models.Processor, referenceProcessorID uuid.UUID) error
-	AddProcessorToFlowAfter(flowID uuid.UUID, processor *models.Processor, referenceProcessorID uuid.UUID) error
+	GetInitialProcessors(flowID uuid.UUID) ([]*SimpleProcessor, error)
+	GetNextProcessors(processorID uuid.UUID) ([]*SimpleProcessor, error)
+	SaveFlowState(flow *SimpleFlow) error
 }
