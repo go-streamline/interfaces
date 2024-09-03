64198dd7acea81939773bd556bbac905bb4b46bf
benyaa <ben2460@gmail.com>
2024-09-03 20:16:28 +0300

    made processor factory simpler

--- definitions/processor_factory.go
+++ definitions/processor_factory.go
@@ -1,7 +1,6 @@
 package definitions
 
-// ProcessorFactory defines an interface for retrieving processors.
-// This can be used to support all sorts of processors.
+// ProcessorFactory defines an interface for retrieving processors and trigger processors.
 type ProcessorFactory interface {
 	// GetProcessor retrieves a processor by its type name.
 	// Parameters:
@@ -9,16 +8,13 @@ type ProcessorFactory interface {
 	// Returns:
 	// - Processor: The retrieved processor.
 	// - error: An error if the retrieval fails.
-	GetProcessor(typeName string) (Processor, error)
+	GetProcessor(typeName string) (*Processor, error)
 
-	// RegisterProcessor registers a new processor.
+	// GetTriggerProcessor retrieves a trigger processor by its type name.
 	// Parameters:
-	// - processor: The processor to register.
-	RegisterProcessor(processor Processor)
-
-	// RegisterProcessorWithTypeName registers a new processor with a specific type name.
-	// Parameters:
-	// - typeName: The type name to associate with the processor.
-	// - processor: The processor to register.
-	RegisterProcessorWithTypeName(typeName string, processor Processor)
+	// - typeName: The type name of the trigger processor to retrieve.
+	// Returns:
+	// - TriggerProcessor: The retrieved trigger processor.
+	// - error: An error if the retrieval fails.
+	GetTriggerProcessor(typeName string) (*TriggerProcessor, error)
 }
