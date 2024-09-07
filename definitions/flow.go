e513575566372e9dbe4a61d21174a08014316daf
benyaa <ben2460@gmail.com>
2024-09-07 18:06:32 +0300

    refactor processors

--- definitions/flow.go
+++ definitions/flow.go
@@ -20,15 +20,15 @@ type Flow struct {
 // SimpleProcessor represents a processor within a flow.
 // It contains metadata about the processor, including its configuration and execution parameters.
 type SimpleProcessor struct {
-	ID         uuid.UUID              `json:"id"`          // ID uniquely identifies the processor.
-	FlowID     uuid.UUID              `json:"flow_id"`     // FlowID is the ID of the flow to which the processor belongs.
-	Name       string                 `json:"name"`        // Name is the human-readable name of the processor.
-	Type       string                 `json:"type"`        // Type indicates the type of the processor, used to determine its implementation.
-	FlowOrder  int                    `json:"flow_order"`  // FlowOrder specifies the order in which the processor is executed within the flow.
-	Config     map[string]interface{} `json:"config"`      // Config holds the specific configuration for the processor.
-	MaxRetries int                    `json:"max_retries"` // MaxRetries specifies the maximum number of retries allowed for this processor in case of failure.
-	LogLevel   logrus.Level           `json:"log_level"`   // LogLevel defines the logging level used for this processor.
-	Enabled    bool                   `json:"enabled"`     // Enabled indicates if the processor is enabled.
+	ID               uuid.UUID              `json:"id"`              // ID uniquely identifies the processor.
+	FlowID           uuid.UUID              `json:"flow_id"`         // FlowID is the ID of the flow to which the processor belongs.
+	Name             string                 `json:"name"`            // Name is the human-readable name of the processor.
+	Type             string                 `json:"type"`            // Type indicates the type of the processor, used to determine its implementation.
+	Config           map[string]interface{} `json:"config"`          // Config holds the specific configuration for the processor.
+	MaxRetries       int                    `json:"max_retries"`     // MaxRetries specifies the maximum number of retries allowed for this processor in case of failure.
+	LogLevel         logrus.Level           `json:"log_level"`       // LogLevel defines the logging level used for this processor.
+	Enabled          bool                   `json:"enabled"`         // Enabled indicates if the processor is enabled.
+	NextProcessorIDs []uuid.UUID            `json:"next_processors"` // NextProcessorIDs explicitly defines the processors that should run after this one.
 }
 
 // SimpleTriggerProcessor represents a trigger processor within a flow.
