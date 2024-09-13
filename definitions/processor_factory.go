b4ef34794bbade0d5de5c18c578d7079db51c2ae
benyaa <ben2460@gmail.com>
2024-09-13 12:49:05 +0300

    updated processor factory interface

--- definitions/processor_factory.go
+++ definitions/processor_factory.go
@@ -4,17 +4,19 @@ package definitions
 type ProcessorFactory interface {
 	// GetProcessor retrieves a processor by its type name.
 	// Parameters:
+	// - id: The unique identifier of the processor.
 	// - typeName: The type name of the processor to retrieve.
 	// Returns:
 	// - Processor: The retrieved processor.
 	// - error: An error if the retrieval fails.
-	GetProcessor(typeName string) (Processor, error)
+	GetProcessor(id string, typeName string) (Processor, error)
 
 	// GetTriggerProcessor retrieves a trigger processor by its type name.
 	// Parameters:
+	// - id: The unique identifier of the trigger processor.
 	// - typeName: The type name of the trigger processor to retrieve.
 	// Returns:
 	// - TriggerProcessor: The retrieved trigger processor.
 	// - error: An error if the retrieval fails.
-	GetTriggerProcessor(typeName string) (TriggerProcessor, error)
+	GetTriggerProcessor(id string, typeName string) (TriggerProcessor, error)
 }
