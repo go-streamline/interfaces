c488ac2ba2e655efa452ece68a830b67c4a49fbb
benyaa <ben2460@gmail.com>
2024-10-24 13:42:45 +0300

    added more docs to coordinator

--- definitions/coordinator.go
+++ definitions/coordinator.go
@@ -2,7 +2,9 @@ package definitions
 
 import "github.com/google/uuid"
 
-// Coordinator defines the interface for coordinating leader election and handling leader failures across a cluster.
+// Coordinator defines the interface for assigning leaders nodes for each trigger processor.
+// The coordinator elects a leader for each trigger processor and monitors the leader nodes.
+// If a node drops, the coordinator is responsible for reassigning the leader.
 type Coordinator interface {
 	// IsLeader checks if the current instance is the leader for the trigger processor.
 	// If the current instance is the coordinator, it will also assign the leader for the trigger processor.
