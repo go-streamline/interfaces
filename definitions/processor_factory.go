cb273f8ea9310ded5dbc6972db8d0b12b3da702b
benyaa <ben2460@gmail.com>
2024-11-01 13:19:51 +0200

    added id to processor factory so future factories can produce state managers for the processors

--- definitions/processor_factory.go
+++ definitions/processor_factory.go
@@ -1,20 +1,24 @@
 package definitions
 
+import "github.com/google/uuid"
+
 // ProcessorFactory defines an interface for retrieving processors and trigger processors.
 type ProcessorFactory interface {
 	// GetProcessor retrieves a processor by its type name.
 	// Parameters:
+	// - id: The ID of the processor to retrieve.
 	// - typeName: The type name of the processor to retrieve.
 	// Returns:
 	// - Processor: The retrieved processor.
 	// - error: An error if the retrieval fails.
-	GetProcessor(typeName string) (Processor, error)
+	GetProcessor(id uuid.UUID, typeName string) (Processor, error)
 
 	// GetTriggerProcessor retrieves a trigger processor by its type name.
 	// Parameters:
+	// - id: The ID of the trigger processor to retrieve.
 	// - typeName: The type name of the trigger processor to retrieve.
 	// Returns:
 	// - TriggerProcessor: The retrieved trigger processor.
 	// - error: An error if the retrieval fails.
-	GetTriggerProcessor(typeName string) (TriggerProcessor, error)
+	GetTriggerProcessor(id uuid.UUID, typeName string) (TriggerProcessor, error)
 }
