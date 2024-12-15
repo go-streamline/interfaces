982e356e929415d63aa748fa6e780c3a0333b37e
benyaa <ben2460@gmail.com>
2024-12-15 21:08:38 +0200

    added logger type

--- definitions/logger_factory.go
+++ definitions/logger_factory.go
@@ -3,5 +3,12 @@ package definitions
 import "github.com/sirupsen/logrus"
 
 type LoggerFactory interface {
-	GetLogger(name string) *logrus.Logger
+	// GetLogger retrieves a logger based on the specified type and name.
+	// Parameters:
+	// - loggerType: The type of logger to retrieve. For example: Human
+	// - name: The name of the instance of the logger. For example, if I have multiple instances for Human,
+	// I can use its name to differentiate between them.
+	// Returns:
+	// - *logrus.Logger: A pointer to the retrieved logger.
+	GetLogger(loggerType string, name string) *logrus.Logger
 }
